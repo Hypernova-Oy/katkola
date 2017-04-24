323421e2b67328f7396b5ec974b38dc1b9c0e10e
Olli-Antti Kivilahti <olli-antti.kivilahti@jns.fi>
2017-04-24 16:07:50 +0300

    SFTP cannot be easily deployed directly to users home with ssh-key auth. Adding sftponly-dir for sftp users only.

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,9 +1,34 @@
 KATKO_INSTALL_DIR="/var/www"
 KATKO_BASE_DIR="$KATKO_INSTALL_DIR/katkola"
+KATKO_SFTP_BASEDIR="/sftponly"
+KATKO_SFTP_BASEDIR_ESC="\/sftponly"
 
 
-useradd -m katkola -s /usr/sbin/nologin
-su -c "cp -r public_html /home/katkola/" -s /bin/bash katkola
+#Create the directory for Kätkölä-users' chrooted dirs.
+mkdir -p "$KATKO_SFTP_BASEDIR"
+chmod 755 "$KATKO_SFTP_BASEDIR"
+
+#Configure sshd to sftp to correct dir
+if [ -e $KATKO_INSTALL_DIR ]; then
+  #Kätkölä sftp already configured
+  echo ""
+else
+  sed "s/ChrootDirectory.*/ChrootDirectory $KATKO_SFTP_BASEDIR_ESC\/%u/" /etc/ssh/sshd_config
+  service sshd restart
+fi
+
+
+if [ $(grep 'katkola' /etc/users) ]; then
+  #Kätkölä-user created
+  echo ""
+else
+  useradd -m katkola -s /usr/sbin/nologin
+  mkdir -p "$KATKO_SFTP_BASEDIR/katkola/private"
+  cp -r public_html "$KATKO_SFTP_BASEDIR/katkola/"
+  chown -R katkola: "$KATKO_SFTP_BASEDIR/katkola"
+  ln -s "$KATKO_SFTP_BASEDIR/katkola/public_html" "/home/katkola/public_html"
+  chown katkola: "/home/katkola/public_html"
+fi
 
 apt-get install apache2
 cp conf/apache2.conf /etc/apache2/sites-available/katkola.conf
